cdc5cbaf73d7a897811251dfda6c1263161748b1
zxcvbnmzsedr <tianzneg@gmail.com>
2019-07-26 15:56:22 +0800

    MajorityElement

---
+++ src/com/ztianzeng/learn/leetcode/MajorityElement.java
@@ -0,0 +1,58 @@
+package com.ztianzeng.learn.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ *
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ *
+ * Example 1:
+ *
+ * Input: [3,2,3]
+ * Output: 3
+ * Example 2:
+ *
+ * Input: [2,2,1,1,1,2,2]
+ * Output: 2
+ *
+ * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+ *
+ * @author zhaotianzeng
+ * @date 2019-07-26 15:32
+ * @version V1.0
+ */
+public class MajorityElement {
+    @Test
+    public void test() {
+        int i = majorityElement(new int[]{3, 2, 3});
+        Assert.assertEquals(i, 3);
+    }
+
+    /**
+     * 从第一个数开始count=1，遇到相同的就加1，遇到不同的就减1，减到0就重新换个数开始计数，总能找到最多的那个
+     *
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int majority = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                majority = nums[i];
+            }
+            if (nums[i] == majority) {
+                count++;
+            } else {
+                count--;
+            }
+
+        }
+        return majority;
+    }
+}
