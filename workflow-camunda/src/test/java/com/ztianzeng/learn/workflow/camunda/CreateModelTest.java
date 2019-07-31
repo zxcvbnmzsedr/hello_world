396ae078d7b3c86ba5935a90fd069c8b43413997
zxcvbnmzsedr <tianzneg@gmail.com>
2019-07-31 14:45:25 +0800

    复杂任务创建

--- workflow-camunda/src/test/java/com/ztianzeng/learn/workflow/camunda/CreateModelTest.java
+++ workflow-camunda/src/test/java/com/ztianzeng/learn/workflow/camunda/CreateModelTest.java
@@ -43,6 +43,40 @@ public class CreateModelTest {
         Bpmn.writeModelToFile(file, modelInstance);
     }
 
+    @Test
+    public void complex() throws IOException {
+        // create an empty model
+        BpmnModelInstance modelInstance = Bpmn.createEmptyModel();
+        Definitions definitions = modelInstance.newInstance(Definitions.class);
+        definitions.setTargetNamespace("http://camunda.org/examples");
+        modelInstance.setDefinitions(definitions);
+
+        Process process = createElement(modelInstance, definitions, "process-with-one-task", Process.class);
+
+        // create elements
+        StartEvent startEvent = createElement(modelInstance, process, "start", StartEvent.class);
+        ParallelGateway fork = createElement(modelInstance, process, "fork", ParallelGateway.class);
+        ServiceTask task1 = createElement(modelInstance, process, "task1", ServiceTask.class);
+        task1.setName("Service Task");
+        UserTask task2 = createElement(modelInstance, process, "task2", UserTask.class);
+        task2.setName("User Task");
+        ParallelGateway join = createElement(modelInstance, process, "join", ParallelGateway.class);
+        EndEvent endEvent = createElement(modelInstance, process, "end", EndEvent.class);
+
+        // create flows
+        createSequenceFlow(modelInstance, process, startEvent, fork);
+        createSequenceFlow(modelInstance, process, fork, task1);
+        createSequenceFlow(modelInstance, process, fork, task2);
+        createSequenceFlow(modelInstance, process, task1, join);
+        createSequenceFlow(modelInstance, process, task2, join);
+        createSequenceFlow(modelInstance, process, join, endEvent);
+
+        // validate and write model to file
+        Bpmn.validateModel(modelInstance);
+        File file = File.createTempFile("bpmn-model-api-", ".bpmn");
+        Bpmn.writeModelToFile(file, modelInstance);
+    }
+
     protected <T extends BpmnModelElementInstance> T createElement(BpmnModelInstance modelInstance,
                                                                    BpmnModelElementInstance parentElement,
                                                                    String id,
