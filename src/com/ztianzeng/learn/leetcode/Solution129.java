e96d13c31c2fadc76459724f2885bb54523d3ffe
zxcvbnmzsedr <tianzneg@gmail.com>
2019-08-08 18:14:40 +0800

    129

---
+++ src/com/ztianzeng/learn/leetcode/Solution129.java
@@ -0,0 +1,93 @@
+package com.ztianzeng.learn.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+ *
+ * An example is the root-to-leaf path 1->2->3 which represents the number 123.
+ *
+ * Find the total sum of all root-to-leaf numbers.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Input: [1,2,3]
+ *     1
+ *    / \
+ *   2   3
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ * Example 2:
+ *
+ * Input: [4,9,0,5,1]
+ *     4
+ *    / \
+ *   9   0
+ *  / \
+ * 5   1
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
+ *
+ * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+ *
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ *
+ * 计算从根到叶子节点生成的所有数字之和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * @author zhaotianzeng
+ * @date 2019-08-08 18:01
+ * @version V1.0
+ */
+public class Solution129 {
+    @Test
+    public void test() {
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(3);
+        Assert.assertEquals(sumNumbers(treeNode), 25);
+    }
+
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return helper(root, 0);
+
+    }
+
+    public int helper(TreeNode root, int parent) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = parent + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+
+        int leftVal = helper(root.left, sum * 10);
+        int rightVal = helper(root.right, sum * 10);
+        return leftVal + rightVal;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
