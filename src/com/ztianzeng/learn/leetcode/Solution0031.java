8004b6ae37869f1244e548ad17138572be8d47c9
zxcvbnmzsedr <tianzneg@gmail.com>
2019-08-29 16:36:03 +0800

    0031

---
+++ src/com/ztianzeng/learn/leetcode/Solution0031.java
@@ -0,0 +1,78 @@
+package com.ztianzeng.learn.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ *
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ *
+ * The replacement must be in-place and use only constant extra memory.
+ *
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ *
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ *实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ *
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ *
+ * 必须原地修改，只允许使用额外常数空间。
+ *
+ * @author zhaotianzeng
+ * @date 2019/8/29 3:41 下午
+ * @version V1.0
+ */
+public class Solution0031 {
+    @Test
+    public void test() {
+        int[] nums = {1, 2, 3};
+        nextPermutation(nums);
+        for (int num : nums) {
+            System.out.print(num);
+        }
+
+    }
+
+    @Test
+    public void test2() {
+        int[] nums = {1, 3, 2};
+        nextPermutation(nums);
+        for (int num : nums) {
+            System.out.print(num);
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
