e15f7f6b45a899d8379b032b22dbb1843e4ab0ea
zxcvbnmzsedr <tianzneg@gmail.com>
2019-09-20 19:23:46 +0800

    0027

--- src/com/ztianzeng/learn/leetcode/Solution0002.java
+++ src/com/ztianzeng/learn/leetcode/Solution0002.java
@@ -27,13 +27,9 @@ import org.junit.Test;
 public class Solution0002 {
     @Test
     public void test() {
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
+        ListNode l1 = ListNode.create(2, 4, 3);
 
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
+        ListNode l2 = ListNode.create(5, 6, 4);
 
         ListNode listNode = addTwoNumbers(l1, l2);
         System.out.println(listNode.val);
