d91e2c77d579ff0c51f5e7c49db45689adb65b87
zxcvbnmzsedr <tianzneg@gmail.com>
2019-09-23 11:36:10 +0800

    中心索引

---
+++ src/com/ztianzeng/learn/leetcode/array/Array_寻找数组的中心索引.java
@@ -0,0 +1,90 @@
+package com.ztianzeng.learn.leetcode.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 给定一个整数类型的数组 nums，请编写一个能够返回数组“中心索引”的方法。
+ *
+ * 我们是这样定义数组中心索引的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+ *
+ * 如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * nums = [1, 7, 3, 6, 5, 6]
+ * 输出: 3
+ * 解释:
+ * 索引3 (nums[3] = 6) 的左侧数之和(1 + 7 + 3 = 11)，与右侧数之和(5 + 6 = 11)相等。
+ * 同时, 3 也是第一个符合要求的中心索引。
+ * 示例 2:
+ *
+ * 输入:
+ * nums = [1, 2, 3]
+ * 输出: -1
+ * 解释:
+ * 数组中不存在满足此条件的中心索引。
+ * 说明:
+ *
+ * nums 的长度范围为 [0, 10000]。
+ * 任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数。
+ * @author zhaotianzeng
+ * @date 2019/9/23 11:19 上午
+ * @version V1.0
+ */
+public class Array_寻找数组的中心索引 {
+    @Test
+    public void test1() {
+        int[] nums = {1, 7, 3, 6, 5, 6};
+        Assert.assertEquals(pivotIndex(nums), 3);
+    }
+
+    public int pivotIndex(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (2 * leftSum == sum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+
+        return -1;
+    }
+
+    public int pivotIndex_1(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int left = i - 1;
+            while (left >= 0) {
+                leftSum += nums[left--];
+            }
+            int right = i + 1;
+            while (right < nums.length) {
+                rightSum += nums[right++];
+            }
+
+            if (leftSum == rightSum) {
+                return i;
+            } else {
+                leftSum = 0;
+                rightSum = 0;
+            }
+        }
+
+
+        return -1;
+    }
+}
