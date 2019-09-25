e5ea46159af1efb981c9fa1dace1095a3fef0a77
zxcvbnmzsedr <tianzneg@gmail.com>
2019-09-25 09:36:49 +0800

    All_转字符串里的单词

---
+++ src/main/java/com/ztianzeng/learn/leetcode/arraystring/All_转字符串里的单词.java
@@ -0,0 +1,59 @@
+package com.ztianzeng.learn.leetcode.arraystring;
+
+import com.ztianzeng.learn.leetcode.utils.PrintUtil;
+import org.junit.Test;
+
+/**
+ * 给定一个字符串，逐个翻转字符串中的每个单词。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入: "the sky is blue"
+ * 输出: "blue is sky the"
+ * 示例 2：
+ *
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 示例 3：
+ *
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ *
+ *
+ * 说明：
+ *
+ * 无空格字符构成一个单词。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ *
+ *
+ * 进阶：
+ *
+ * 请选用 C 语言的用户尝试使用 O(1) 额外空间复杂度的原地解法。
+ * @author zhaotianzeng
+ * @date 2019/9/25 9:31 上午
+ * @version V1.0
+ */
+public class All_转字符串里的单词 {
+    @Test
+    public void test1() {
+        String the_sky_is_blue = reverseWords("the sky is blue");
+        PrintUtil.print(the_sky_is_blue);
+    }
+
+    public String reverseWords(String s) {
+        String[] s1 = s.trim().split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = s1.length - 1; i >= 0; i--) {
+            if (s1[i].trim().equals("")) {
+                continue;
+            }
+            sb.append(s1[i].trim()).append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
