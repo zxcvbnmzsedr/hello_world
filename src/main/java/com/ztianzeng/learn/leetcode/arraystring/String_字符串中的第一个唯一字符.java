fb6de87f5aa28a94cdb5d2ce9a7005af08d46ff1
zxcvbnmzsedr <tianzneg@gmail.com>
2019-09-26 09:17:04 +0800

    更换数据结构，效率提升巨大

--- src/main/java/com/ztianzeng/learn/leetcode/arraystring/String_字符串中的第一个唯一字符.java
+++ src/main/java/com/ztianzeng/learn/leetcode/arraystring/String_字符串中的第一个唯一字符.java
@@ -3,8 +3,6 @@ package com.ztianzeng.learn.leetcode.arraystring;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashMap;
-
 /**
  * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
  *
@@ -47,13 +45,12 @@ public class String_字符串中的第一个唯一字符 {
         if (chars.length == 0) {
             return -1;
         }
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < chars.length; i++) {
-            map.put(chars[i], map.getOrDefault(chars[i], 0) + 1);
+        int[] map = new int[256];
+        for (char aChar : chars) {
+            map[aChar] += 1;
         }
         for (int i = 0; i < chars.length; i++) {
-            if (map.get(chars[i]) == 1) {
+            if (map[chars[i]] == 1) {
                 return i;
             }
         }
