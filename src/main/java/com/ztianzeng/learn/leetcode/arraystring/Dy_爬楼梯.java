6b77d02f088ad74d86463be7a8b6ecba922d75f6
zxcvbnmzsedr <tianzneg@gmail.com>
2019-09-28 19:30:17 +0800

    爬楼梯

---
+++ src/main/java/com/ztianzeng/learn/leetcode/arraystring/Dy_爬楼梯.java
@@ -0,0 +1,77 @@
+package com.ztianzeng.learn.leetcode.arraystring;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ *
+ * @author zhaotianzeng
+ * @date 2019/9/28 6:21 下午
+ * @version V1.0
+ */
+public class Dy_爬楼梯 {
+    @Test
+    public void test() {
+        int i = climbStairs(3);
+        Assert.assertEquals(i, 3);
+    }
+
+    @Test
+    public void test2() {
+        int i = climbStairs(4);
+        Assert.assertEquals(i, 5);
+    }
+
+    @Test
+    public void test4() {
+        int i = climbStairs_1(44);
+        Assert.assertEquals(i, 1134903170);
+    }
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+
+    public int climbStairs_1(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+}
